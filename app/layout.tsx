fa6b4ecab9472c5f9e0e8d422b02b8f1f5d87401
creektechno24 <creektechno24@gmail.com>
2026-05-08 20:00:18 +0530

    Updated logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,31 +74,26 @@ export const metadata: Metadata = {
   generator: 'v0.app',
 
   // ✅ FAVICONS
-  icons: {
+ icons: {
 
-    icon: [
+  icon: [
 
-      {
-        url: "/favicon.ico"
-      },
+    {
+      url: "/favicon.ico",
+    },
 
-      {
-        url: "/favicon-96x96.png",
-        sizes: "96x96",
-        type: "image/png"
-      },
+    {
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
 
-      {
-        url: "/favicon.svg",
-        type: "image/svg+xml"
-      },
+  ],
 
-    ],
+  apple:
+    "/apple-touch-icon.png",
 
-    apple:
-      "/apple-touch-icon.png",
-
-  },
+},
 
 }
 
