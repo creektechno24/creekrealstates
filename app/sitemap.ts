ccae109796e51402e02eaa43d3db5465331cddb0
creektechno24 <creektechno24@gmail.com>
2026-05-11 10:23:37 +0530

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,6 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next"
 
-export default function sitemap():
-  MetadataRoute.Sitemap {
+export default function sitemap(): MetadataRoute.Sitemap {
 
   return [
 
@@ -19,5 +18,34 @@ export default function sitemap():
       priority: 0.9,
     },
 
-  ];
+    {
+      url: "https://creekrealestates.com/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: "https://creekrealestates.com/terms-and-conditions",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: "https://creekrealestates.com/website-usage-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: "https://creekrealestates.com/disclaimer",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+  ]
+
 }
