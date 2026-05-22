155e0c1764d92667090aff822247f1b0f16152a0
creektechno24 <creektechno24@gmail.com>
2026-05-22 22:08:13 +0530

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  images: {
-
-    // ✅ disable optimization for uploaded images
-    unoptimized: true,
-
-    remotePatterns: [
-
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-
-    ],
-
-    formats: [
-      "image/avif",
-      "image/webp",
-    ],
-
-    minimumCacheTTL: 60,
-
-  },
-
-  compress: true,
-
-  poweredByHeader: false,
-
-  reactStrictMode: true,
-
-}
+const nextConfig = {}
 
 export default nextConfig
