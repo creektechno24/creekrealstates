2b31eac6a7a979ce0b3549f2413020d0879ab0c2
creektechno24 <creektechno24@gmail.com>
2026-05-24 00:40:06 +0800

    update next.m.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",
-      "res.cloudinary.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
